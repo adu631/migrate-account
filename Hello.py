147f606004f5c2f2f3c1d0f0f1660db7993ff02c
adityasquad <127858841+adityasquad@users.noreply.github.com>
2024-02-24 04:17:22 +0530

    add_users-updated

--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
+import requests
+import json
 friends = ['Taylor', 'Alex', 'Pat', 'Eli']
 for friend in friends:
     print("Hi " + friend)
@@ -7,4 +9,32 @@ for i in range(10):
 
 print((((1+2)*3)/4)**5)
 
-print("The average size is: " + str(average)) #implicit conversion
+#print("The average size is: " + str(average)) #implicit conversion
+bearer_token_post= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDg4OTU5MDksImp0aSI6IjIzMzM3MjIyNDhiNDE3NmRhNGY0ODhjYTE2YTM4MzcwZmIyYTI1NjBiY2IyZGI0M2NiNDM4YWUxNzI3MDhmMWMiLCJpYXQiOjE3MDg3MjMxMDksImlzcyI6ImFwaS5zcXVhZGNhc3QuY29tIiwibmJmIjoxNzA4NzIzMTA5LCJpZCI6IjY1ZDhmYTE0YzBlYjlmZTUzMTczNzVjZCIsImZpcnN0X25hbWUiOiJCYXJyeSAiLCJlbWFpbCI6InNxdWFkQHNxdWFkMi5jb20iLCJ1c2VyIjpudWxsLCJzc29fbG9naW4iOmZhbHNlLCJzc29fdG9rZW4iOiIiLCJvcmdhbml6YXRpb25faWQiOiI2NWQ4ZmEyMGMwZWI5ZmU1MzE3Mzc1Y2UiLCJvcmdhbml6YXRpb24iOm51bGwsInJlZnJlc2hfdG9rZW4iOiJhYmQ3NDNjZDkwYTJkZDBhN2NkMjJmMjRlNGU3YmY0MDY2NmVjZWUzM2E3NmY2ZmQ1ODg2MTEwOWJhYzdjMDgzMzllN2MwZDdmMDhhNWFiZjlmMGYxZjVlOTdiOWI4M2NjMzk0OTVjYzQyMjIxNzkwMDNlZTViNzk0ZjM5Y2JmOCIsIndlYl90b2tlbiI6ZmFsc2UsImFwaV90b2tlbiI6dHJ1ZX0.1ek8I76Chcu4lNn7DZ_fSvn50HkJ1Ju2YCmECNtG3EA"
+
+def post_users(email, role, firstname, lastname):
+  url = "https://api.squadcast.com/v3/users"
+  if(role=="account_owner"):
+    roles= "user"
+  else:
+    roles = role
+  payload = {
+    "email": email,
+    "role": roles,
+    "first_name": firstname,
+    "last_name": lastname           
+             }
+  headers1 = {'Authorization': "Bearer "+str(bearer_token_post),'Content-Type': 'application/json'}
+  response = requests.request("POST", url, headers=headers1, data=payload)
+  try:
+    response = requests.request("POST", url, headers=headers1, json=payload)
+    response.raise_for_status()
+    print(response.text)  # Print the raw response
+    res = response.json()
+    print(res)
+  except requests.exceptions.RequestException as e:
+    print(f"Request failed: {e}")
+    res = response.json()
+  print(res)
+
+post_users("aditya@squadcast.com","user","Aditya","Anil")
--- add_users.py
+++ add_users.py
@@ -1,27 +1,35 @@
 import requests
 import json
-get_user_token="b4a2de906b0c2c69e88bcc4f56add43685f7eeccf13bab79bdc324499403137ad3c1b450044856628814fed6688da2b77fe285ec32c22fd7852f78674a5d39a3"
-post_user_token = "965e4feaee2841f9d90c7a835f5c9875b3ec1cbba7ea20235aa9498ffce9434d1be03b993557faef7b4fdfc41b52f33a6d5c256aa8027525306e377b39842977"
+get_user_token="965e4feaee2841f9d90c7a835f5c9875b3ec1cbba7ea20235aa9498ffce9434d1be03b993557faef7b4fdfc41b52f33a6d5c256aa8027525306e377b39842977"
+post_user_token = "c09b4aefabfc5aee801b9bb1e157461644c2cfd66f2050d9d85db3242b2e53d638d21edadfc2146e78be570db78242e7a12748ac43678501df8cef6ddfd439d2"
 payload = {}
-get_headers = {
+"""get_headers = {
   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDgwNzk2MzgsImp0aSI6IjMyMWFjNTcxYWI2NmNhODIzMGI3MDYwMzY1OTM2YzljN2M3YTQyZTIxZWE0NWU3ZGIzM2RhYjc4Yjg0MjA2N2IiLCJpYXQiOjE3MDc5MDY4MzgsImlzcyI6ImFwaS5zcXVhZGNhc3QuY29tIiwibmJmIjoxNzA3OTA2ODM4LCJpZCI6IjYxMmY5YmJjNGFmYjE0MDAwNzMxNDU5MCIsImZpcnN0X25hbWUiOiJBa2FzaCIsImVtYWlsIjoiYWthc2hAc3F1YWRjYXN0LmNvbSIsInVzZXIiOm51bGwsInNzb19sb2dpbiI6ZmFsc2UsInNzb190b2tlbiI6IiIsIm9yZ2FuaXphdGlvbl9pZCI6IjVkMTM1MzU5MDA4NjA0MDAxMmVhODg5ZSIsIm9yZ2FuaXphdGlvbiI6bnVsbCwicmVmcmVzaF90b2tlbiI6IjdkZmYwNmEzZjQwM2VhNTg5Mzc2NzJjZDk4ZDU5MWUwMjJhMWI4OGUzN2Q2ZjJhYzZmYTU3YjdiYjQ0MjBmYjEzNDFlMzhiYTIwOTYxOWVjYmIzOTgxMTRkNTBmZmU3MTQ0MDNlMzY5Nzg2OGFmMWZmMDc3MDlhMTE4NDljOGUyIiwid2ViX3Rva2VuIjpmYWxzZSwiYXBpX3Rva2VuIjp0cnVlfQ.Fsin09O3f1Ysq8BMWp0G8Xacy_mJATpWkSRu4G9HI-E'
 }
 post_headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDgwNzgwNDAsImp0aSI6IjhkYTEwMzA2NWFkZWZmYTI3ZDI3ZmVmYTIzNjQ0Y2VmNzRhNDViMTYzODQ4NTM2OTRjZWRmZGNmOWRiNjFjYmMiLCJpYXQiOjE3MDc5MDUyNDAsImlzcyI6ImFwaS5zcXVhZGNhc3QuY29tIiwibmJmIjoxNzA3OTA1MjQwLCJpZCI6IjYxMmY5YmJjNGFmYjE0MDAwNzMxNDU5MCIsImZpcnN0X25hbWUiOiJBa2FzaCIsImVtYWlsIjoiYWthc2hAc3F1YWRjYXN0LmNvbSIsInVzZXIiOm51bGwsInNzb19sb2dpbiI6ZmFsc2UsInNzb190b2tlbiI6IiIsIm9yZ2FuaXphdGlvbl9pZCI6IjY1Y2M4NzA4OGE2MWI1NjZkZTQ3YTg0NSIsIm9yZ2FuaXphdGlvbiI6bnVsbCwicmVmcmVzaF90b2tlbiI6IjVlZmFkMGQyMDEzZTE0MTRjNTk0MGJmMWE3ZjAzNmMwN2NiMmZiMWE3MmQ3NjhhODk1YTU4Nzg5MjRkODRmZjUxM2E5ZmFmNjE1OTVmMWRiODhjNzhhZDNmOTBhOTkzMzNlNDRhNDlmOTU1NGI3ZTFjZDBlYjlkOTUxNzE3NjQwIiwid2ViX3Rva2VuIjpmYWxzZSwiYXBpX3Rva2VuIjp0cnVlfQ.hRkwN9zOriopzBuqHSi9l-CE0eX5C7wkZfhTo8YGtU8'
-}
+}"""
+
+def handle_request(method, url, headers, data):
+    try:
+        response = requests.request(method, url, headers=headers, data=data)
+        response.raise_for_status()  # Raises HTTPError for bad responses
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
 
 def get_auth_token():
   url = "https://auth.squadcast.com/oauth/access-token"
-  response = requests.request("GET", url, headers = {"X-Refresh-Token": str(get_user_token)}, data=payload)
-  res = response.json()
-  return res
+  response = handle_request("GET", url, headers = {"X-Refresh-Token": str(get_user_token)}, data=payload)
+  return response
 
 def post_auth_token():
   url = "https://auth.squadcast.com/oauth/access-token"
-  response = requests.request("GET", url, headers = {"X-Refresh-Token": str(post_user_token)}, data=payload)
-  res = response.json()
-  return res
+  response = handle_request("GET", url, headers = {"X-Refresh-Token": str(post_user_token)}, data=payload)
+  return response
+
 token = get_auth_token()
 bearer_token_get = token["data"]["access_token"]
 #print(bearer_token_get)
@@ -29,23 +37,68 @@ bearer_token_get = token["data"]["access_token"]
 token = post_auth_token()
 bearer_token_post = token["data"]["access_token"]
 #print(bearer_token_post)
+#print(bearer_token_post)
 
 def get_users():
   url = "https://api.squadcast.com/v3/users"
-  response = requests.request("GET", url, headers = {'Authorization': "Bearer "+str(bearer_token_get)})
-  res = response.json()
-  return res
+  response = handle_request("GET", url, headers = {'Authorization': "Bearer "+str(bearer_token_get)},data=payload)
+  return response
 
 get_users1 = get_users()
 
-for x in get_users1["data"]:
-  print (get_users1["data"][x])
+#for x in get_users1["data"]:
+  #print (get_users1["data"][x])
 
 def get_team():
   url = "https://api.squadcast.com/v3/teams/:teamID"
   response = requests.request("GET", url, headers = {'Authorization': "Bearer "+str(bearer_token_get)})
   res = response.json()
   return res
+
+def post_users(email, role, firstname, lastname):
+  url = "https://api.squadcast.com/v3/users"
+  if(role=="account_owner"):
+    roles= "user"
+  else:
+    roles = role
+  payload = {
+    "email": email,
+    "role": roles,
+    "first_name": firstname,
+    "last_name": lastname           
+             }
+  headers1 = {'Authorization': "Bearer "+str(bearer_token_post),'Content-Type': 'application/json'}
+  #handle_request("POST", url, headers=headers1,data=payload)
+  try:
+    response = requests.request("POST", url, headers=headers1, json=payload)
+    response.raise_for_status()
+    print(response.text)  # Print the raw response
+    res = response.json()
+    print(res)
+  except requests.exceptions.RequestException as e:
+    print(f"Request failed: {e}")
+    res = response.json()
+  print(res)
+  """response = requests.request("POST", url, headers=headers1, data=payload)
+  res = response.json()
+  print(res)"""
+  
+
+def migrate_users():
+  users = get_users()
+  for x in users["data"]:
+    email = x["email"]
+    role  = x["role"]
+    firstname = x["first_name"]
+    lastname = x["last_name"]
+    print(email+" "+role+" "+firstname+" "+lastname)
+    post_users(email, role, firstname, lastname)
+
+migrate_users()
+    
+
+
+   
 
   
 #print( get_users1)
